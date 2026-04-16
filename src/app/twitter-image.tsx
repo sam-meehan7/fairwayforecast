a86279a7e907393429d24d6b02eb8a4d50debe5e
Sam <sammeehansam@gmail.com>
2026-04-17 00:37:30 +0100

    fix

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,7 +1,10 @@
-export {
-  default,
-  runtime,
-  alt,
-  size,
-  contentType,
-} from "./opengraph-image";
+import { ogAlt, ogContentType, ogSize, renderOgImage } from "./_og-image";
+
+export const runtime = "edge";
+export const alt = ogAlt;
+export const size = ogSize;
+export const contentType = ogContentType;
+
+export default async function TwitterImage() {
+  return renderOgImage();
+}
